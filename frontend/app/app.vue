3c3d88a2d399c9898416125df142ac47f09fa5e2
morishitakosuke1028 <k.morishita1028@outlook.jp>
2025-10-04 09:26:49 +0900

    feat: tailwindcssモジュールインストール

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <div class="font-red-500 text-center text-3xl">Nuxt Tailwind</div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,5 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    cssPath: ["~/app/assets/css/tailwind.css", { injectPosition: "first" }], // Default
+    config: {
+      content: [
+        "~/app/components/**/*.{js,vue,ts}",
+        "~/app/layouts/**/*.vue",
+        "~/app/pages/**/*.vue",
+        "~/app/plugins/**/*.{js,ts}",
+        "~/app/app.vue",
+        "~/app/error.vue",
+      ],
+    },
+    viewer: true,
+  },
 })
